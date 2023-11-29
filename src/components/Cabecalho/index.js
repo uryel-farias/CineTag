289c32bc92aaf5d737b85053fc54e9fdadc34571
Uryel Farias <uryel.farias@gmail.com>
2023-11-29 07:11:01 -0300

    Primeiro Comit
    Base do projeto iniciada, com alguns componentes já criados, como o Cabeçalho, Banner, Título e Rodapé.

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import logo from './logo.png'
+import styles from './Cabecalho.module.css'
+import CabecalhoLink from "components/CabecalhoLink";
+
+function Cabecalho(){
+    return(
+        <header className={styles.Cabecalho}>
+            <Link to="./">
+                <img src={logo} alt="Logo do cinetag" />
+            </Link>
+
+            <nav>
+                <CabecalhoLink url="./">
+                    Home
+                </CabecalhoLink>
+
+                <CabecalhoLink url="./Favoritos">
+                    Favoritos
+                </CabecalhoLink>
+            </nav>
+        </header>
+    )
+}
+
+export default Cabecalho;
---
+++ src/components/Rodape/index.js
@@ -0,0 +1,13 @@
+import styles from './Rodape.module.css'
+
+function Rodape() {
+    return(
+        <footer className={styles.rodape}>
+            <p>Desenvolvido por Alura</p>
+            <p>Reproduzido por Uryel</p>
+        </footer>
+    )
+
+}
+
+export default Rodape
